eb82d34c1d111738e974ea6f55592aa64e068252
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2021-04-26 18:52:09 -0400

    promise

---
+++ fundamentos/promesas.js
@@ -0,0 +1,42 @@
+const empleados = [
+	{
+		id: 1,
+		nombre: 'Fernando'
+	},
+	{
+		id: 2,
+		nombre: 'linda'
+	},
+	{
+		id: 3,
+		nombre: 'kare'
+	}
+];
+const salarios = [
+	{
+		id: 1,
+		salario: 1000
+	},
+	{
+		id: 2,
+		salario: 1500
+	}
+];
+const getEmpleado = (id) => {
+	
+	const promesa = new Promise(resolve, reject) => {
+		const empleado = empleados.find((e) =>  e.id === id);
+			if(empleado){
+				resolve(empleado);
+				//return empleado;
+			}else{
+				reject(`Empleado con id ${id} no existe`);
+				//return `Empleado con id ${id} no existe`;
+			}
+	}
+	return promesa;
+}
+const id = 2;
+getEmpleado(id)
+	.then(empleado => console,log(empleado))
+	.catch(err => console.log(err));
