58d925a9e0a1e70ae5fbf97ee41051928d23c8e3
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2021-04-26 22:30:50 -0400

    base 6

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,6 @@
 const {getMultiplicacion} = require('./helpers/multiplicar');
 console.clear();
-const n = 3;
+const n = 6;
 getMultiplicacion(n)
 	.then(multiplicar => console.log(multiplicar, 'creado'))
 	.catch(err => console.log(err));
