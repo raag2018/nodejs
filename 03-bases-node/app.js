0d78b8b01bb8489c795a949a5d63ac79dda7e562
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2021-04-26 22:50:23 -0400

    crear archivo en appjs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,6 @@
 const {crearArchivo} = require('./helpers/multiplicar');
 console.clear();
 const n = 3;
-getMultiplicacion(n)
+crearArchivo(n)
 	.then(multiplicar => console.log(multiplicar, 'creado'))
 	.catch(err => console.log(err));
