3ea382486507be4cc77f7b4ca3516b853e21807d
Roberto Antonio Alferes Gomez <roberto3652017@gmail.com>
2021-04-30 02:34:10 -0400

    process argv

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,9 @@
 const {crearArchivo} = require('./helpers/multiplicar');
 console.clear();
+const [,,arg3 = 5] = process.argv;
+const [,base = 5] = arg3.split("=");
+console.log(base);
 const n = 3;
-crearArchivo(n)
+crearArchivo(base)
 	.then(multiplicar => console.log(multiplicar, 'creado'))
 	.catch(err => console.log(err));
